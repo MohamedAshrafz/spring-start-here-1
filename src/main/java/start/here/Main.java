c42c4e1daedae5dd52857c5901dbcebfbc1dfc66
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2025-04-27 00:12:19 +0300

    multiple beans of the same type and how to retrieve each one

--- src/main/java/start/here/Main.java
+++ src/main/java/start/here/Main.java
@@ -3,12 +3,24 @@ package start.here;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import static start.here.ProjectConfig.PARROT_JAKO_NAME;
+import static start.here.ProjectConfig.PARROT_KOKO_NAME;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+//        System.out.println(context.getBean(Parrot.class));
+
+        // You cannot define multiple beans from the same type(class), If you tried you will get an exception
+        // If you have @Primary above a bean it will be returned(when multiple beans of the same type exist)
         System.out.println(context.getBean(Parrot.class));
-        System.out.println(context.getBean(Parrot.class));
-        System.out.println(context.getBean(Parrot.class));
+
+        /**
+         To get the beans you want, you need to specify which one using the <b>bean name</b>
+         **/
+
+        System.out.println(context.getBean(PARROT_KOKO_NAME, Parrot.class));
+        System.out.println(context.getBean(PARROT_JAKO_NAME, Parrot.class));
     }
 }
--- src/main/java/start/here/ProjectConfig.java
+++ src/main/java/start/here/ProjectConfig.java
@@ -2,20 +2,49 @@ package start.here;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
-    @Bean
-    Parrot Koko() {
+    public enum BEAN_NAMES {
+        PARROT_KOKO("Parrot Koko Bean"),
+        PARROT_JAKO("Parrot Jako Bean");
+        public final String name;
+
+        BEAN_NAMES(String name) {
+            this.name = name;
+        }
+    }
+
+    public static final String PARROT_KOKO_NAME = "Parrot Koko Bean";
+    public static final String PARROT_JAKO_NAME = "Parrot Jako Bean";
+
+    //    @Bean(value = "Parrot koko bean")
+//    @Bean(name = PARROT_KOKO_NAME)
+    @Bean(PARROT_KOKO_NAME)
+    // The default bean if you didn't specify the bean name you want
+    @Primary
+    Parrot b1() {
         var p = new Parrot();
         p.setName("Koko");
+
+        System.out.printf("Parrot %s was created%n", p.getName());
+        return p;
+    }
+
+    @Bean(PARROT_JAKO_NAME)
+    Parrot b2() {
+        var p = new Parrot();
+        p.setName("Jako");
+
+        System.out.printf("Parrot %s was created%n", p.getName());
         return p;
     }
 }
 
-class Parrot{
-    public String name;
+class Parrot {
+    private String name;
 
     public Parrot() {
         name = "No name provided";
@@ -33,4 +62,10 @@ class Parrot{
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return "Parrot{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
