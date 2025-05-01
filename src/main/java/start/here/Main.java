1789253a500bafca0e331eb5445fd3761ffc8da8
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2025-05-02 00:59:16 +0300

    Apply ordering over aspects allowing aspect chaining

--- src/main/java/start/here/Main.java
+++ src/main/java/start/here/Main.java
@@ -15,9 +15,7 @@ public class Main {
 
         CommentService commentService = context.getBean(CommentService.class);
 
-        commentService.publishComment("Hi there");
         commentService.deleteComment("Hi there");
-        commentService.editComment("Hi there");
 
 
 //        // Stereotype Annotation
--- src/main/java/start/here/aspects/SecurityAspect.java
+++ src/main/java/start/here/aspects/SecurityAspect.java
@@ -1,31 +1,28 @@
 package start.here.aspects;
 
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Aspect
 @Component
-@Order(2)
-public class LoggingAspect {
+@Order(1)
+public class SecurityAspect {
 
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
     @Around("@annotation(ToLog)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.info("Log Aspect: Invoked");
+    public Object secureMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Security Aspect: Invoked");
 
         // Will return nothing, but you can keep it as nothing is still be a null object
         Object returnedVal = joinPoint.proceed();
 
-        logger.info("Log Aspect: Finished");
+        logger.info("Security Aspect: Finished");
 
         return returnedVal;
     }
